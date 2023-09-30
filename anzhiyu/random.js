58e14dec21f67c512035247266c292dbed9b5294
hahaCoder <1306846969@qq.com>
2023-09-30 23:54:44 +0800

    Site updated: 2023-09-30 23:54:44

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/05/17/chap01/","2023/05/17/chap04/","2023/05/17/chap02/","2023/05/17/chap05/","2023/05/17/chap03/","2023/09/30/chap06/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/05/17/chap02/","2023/05/17/chap01/","2023/05/17/chap03/","2023/05/17/chap04/","2023/05/17/chap05/","2023/09/30/chap06/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
